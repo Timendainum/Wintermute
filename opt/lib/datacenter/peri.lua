245550b33b3d96918f1fe63fc87022ef80f275ef
Timendainum <timendainum@hotmail.com>
2013-11-29 20:12:12 -0600

    added coreperi

--- opt/lib/datacenter/peri.lua
+++ opt/lib/datacenter/peri.lua
@@ -4,7 +4,7 @@
 -- main program
 ---------------------------------------------------
 -- peripherals
-monitor1 = periHelper.wrapUp("monitor_1")
+monitor1 = nperi.wrap("coreperi", "monitor_1")
 drive2 = periHelper.wrapUp("drive_2")
 --drive3 = periHelper.wrapUp("drive_3")
 printer1 = periHelper.wrapUp("printer_1")
