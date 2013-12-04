2b24e224ea3985b8aa08fda592ba91724bba14d6
Timendainum <timendainum@hotmail.com>
2013-12-03 22:07:40 -0600

    adding more apis

--- boot/apiloader.lua
+++ boot/apiloader.lua
@@ -12,8 +12,10 @@ os.loadAPI("opt/lib/net")
 os.loadAPI("opt/lib/netfile")
 
 --other
+os.loadAPI("opt/lib/file")
 os.loadAPI("opt/lib/nets")
 os.loadAPI("opt/lib/nperi")
+os.loadAPI("opt/lib/peri")
 if fs.exists("ocs/apis/sensor") then
 	os.loadAPI("ocs/apis/sensor")
 end
