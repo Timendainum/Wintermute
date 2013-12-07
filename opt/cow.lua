c82eb8b7a70027dac1d7bfded1c202f61c826ad6
Timendainum <timendainum@hotmail.com>
2013-12-06 19:19:05 -0600

    cows

--- opt/cow.lua
+++ opt/cow.lua
@@ -1,7 +1,7 @@
 -- cow control program
 local server = "cowsensor2"
 local side = "top"
---local cowSensor = nperi.wrap(server, side)
+local cowSensor = nperi.wrap(server, side)
 local cows = {}
 
 local function updateCows()
@@ -32,65 +32,7 @@ end
 
 ------------------------------------------
 --
---
-txt.sPrint("IsPresent: ", nperi.isPresent(server, side))
-local p = nperi.wrap(server, side)
-txt.sPrint("Wrap: ", p)
-local tm = nperi.getMethods(server, side)
-if tm ~= nil then
-	textutils.tabulate(tm)
-else
-	print("getMethods nil")
-end
-
-local tt = p.getTargets()
-
-txt.sPrint(tt)
-
-if tt ~= nil then
-	print(serial.serialize(tt))
-else
-	print("getMethods nil")
-end
-
-
-
-
---[[
 while true do
 	updateCows()
 	txt.sPrint("There are ", countCows(), " cows in the farm.")
 end
-
-
-
-
-
-
--- test
-txt.sPrint("IsPresent: ", peripheral.isPresent("top"))
-local p = sensor.wrap("top")
-txt.sPrint("Wrap: ", p)
-local tm = peripheral.getMethods("top")
-if tm ~= nil then
-	textutils.tabulate(tm)
-else
-	print("getMethods nil")
-end
-
-local tt = p.getTargets()
-
-txt.sPrint(tt)
-
-if tt ~= nil then
-	print(serial.serialize(tt))
-else
-	print("getMethods nil")
-end
-]]--
-
-
-
-
-
-
