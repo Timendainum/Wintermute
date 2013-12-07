5ec47d1fa7461e1b58950cf6af25e232f5d34fb2
Timendainum <timendainum@hotmail.com>
2013-12-06 20:34:52 -0600

    cow

--- opt/cow.lua
+++ opt/cow.lua
@@ -1,10 +1,39 @@
+--0 white
+--1 orange
+--2 magenta
+--3 lightBlue
+--4 yellow
+--5 lime
+--6 pink
+--7 gray
+--8 lightGray
+--9 cyan
+--10 purple
+--11 blue
+--12 brown
+--13 green
+--14 red
+--15 black
+
 -- cow control program
 local server = "cowsensor2"
 local side = "top"
+local redSide = "left"
 local cowSensor = nperi.wrap(server, side)
+local mon = peripheral.wrap("monitor_7")
+
 local cows = {}
+local switches = {}
+switches.spawner = colors.orange
+switches.breeder = colors.magenta
+switches.vet = colors.lightBlue
+switches.rancher = colors.yellow
+switches.grinder = colors.lime
+switches.slaughter = colors.pink
+
 
 local function updateCows()
+	debug.log(30, "Updating cows...")
 	local tResults = cowSensor.getTargets()
 	if tResults ~= nil then
 		cows = {}
@@ -14,7 +43,7 @@ local function updateCows()
 			end
 		end
 	else
-		print("Unable to update cows, sensor result nil.")
+		debug.log(1, "Unable to update cows, sensor result nil.")
 	end
 end
 
@@ -30,9 +59,61 @@ local function countCows()
 	end
 end
 
+local function getSetting(color)
+	return colors.test(redstone.getBundledInput(redSide), color)
+end
+
+local function setSetting(color, setting)
+	if setting then
+		redstone.setBundledOutput(redSide, colors.combine(redstone.getBundledOutput(redSide), color))
+	else
+		redstone.setBundledOutput(redSide, colors.subtract(redstone.getBundledOutput(redSide), color))
+	end
+end
+
 ------------------------------------------
---
---while true do
+-- Main
+
+-- Default switch settings:
+for k,v in ipairs(switches) do
+	setSetting(v, false)
+end
+
+-- 29, 12
+mon.clear()
+mon.setCursorPos(1,1)
+txt.centerWrite(mon, "Cow Breeder Control")
+mon.setCursorPos(1,2)
+mon.write("--Data-----------------------")
+mon.setCursorPos(1,4)
+mon.write("--Settings-------------------")
+
+while true do
 	updateCows()
-	txt.sPrint("There are ", countCows(), " cows in the farm.")
---end
+	local cowCount = tonumber(countCows())
+	debug.log(20, "There are ", cowCount, " cows in the farm.")
+	
+	mon.setCursorPos(3,3)
+	mon.clearLine()
+	mon.write("Cows: ", .. tostring(cowCount))
+
+	mon.setCursorPos(3, 5)
+	mon.clearLine()
+	mon.write("Spawner: " .. tostring(getSetting(switches.spawner)))
+	mon.setCursorPos(18, 5)
+	mon.write("Rancher: " .. tostring(getSetting(switches.rancher)))
+	
+	mon.setCursorPos(3, 6)
+	mon.clearLine()
+	mon.write("Breeder: " .. tostring(getSetting(switches.breeder)))
+	mon.setCursorPos(18, 6)
+	mon.write("Grinder: " .. tostring(getSetting(switches.grinder)))
+
+	mon.setCursorPos(3, 7)
+	mon.clearLine()
+	mon.write("Vet: " .. tostring(getSetting(switches.vet)))
+	mon.setCursorPos(18, 7)
+	mon.write("Slaughter: " .. tostring(getSetting(switches.slaughter)))
+	
+	sleep(5)
+end
